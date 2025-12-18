7f07cf325624236b414a72dcf7fc929532f635aa
oogushi-ryota <r.o.0304.skj@gmail.com>
2025-12-19 00:54:27 +0900

    titleタグ出力

--- wp/functions.php
+++ wp/functions.php
@@ -59,4 +59,10 @@ add_action('template_redirect', function() {
     }
 });
 
+// title-tag サポートを有効にする
+function my_theme_support() {
+    add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'my_theme_support' );
+
 ?>
